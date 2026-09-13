a71e5458ffdffb29ba1efcf832c24bcf327ada15
takaritu2011212-lang <takaritu2011212@gmail.com>
2026-09-14 08:16:52 +0900

    feat: support delayed bonus payout records

--- js/storage.js
+++ js/storage.js
@@ -8,11 +8,8 @@ class GameStorage {
 
     loadData() {
         const data = localStorage.getItem(this.STORAGE_KEY);
-        if (data) {
-            this.data = JSON.parse(data);
-        } else {
-            this.data = this.getDefaultData();
-        }
+        if (data) this.data = JSON.parse(data);
+        else this.data = this.getDefaultData();
     }
 
     getDefaultData() {
@@ -34,19 +31,9 @@ class GameStorage {
         };
     }
 
-    saveData() {
-        localStorage.setItem(this.STORAGE_KEY, JSON.stringify(this.data));
-    }
-
-    resetData() {
-        this.data = this.getDefaultData();
-        this.saveData();
-    }
-
-    setSetting(setting) {
-        this.data.setting = setting;
-        this.saveData();
-    }
+    saveData() { localStorage.setItem(this.STORAGE_KEY, JSON.stringify(this.data)); }
+    resetData() { this.data = this.getDefaultData(); this.saveData(); }
+    setSetting(setting) { this.data.setting = setting; this.saveData(); }
 
     addGame(bet, payout) {
         this.data.totalGames++;
@@ -56,73 +43,43 @@ class GameStorage {
         this.saveData();
     }
 
-    addCredit(amount) {
+    // 前兆後のボーナス告知など、ゲーム数を増やさず払出だけを記録する。
+    addPayout(amount) {
+        if (!Number.isFinite(amount) || amount <= 0) return;
+        this.data.totalPayout += amount;
         this.data.credit += amount;
         this.saveData();
     }
 
-    addBig() {
-        this.data.bigCount++;
-        this.saveData();
-    }
-
-    addReg() {
-        this.data.regCount++;
-        this.saveData();
-    }
-
-    addEpisode() {
-        this.data.episodeCount++;
-        this.saveData();
-    }
-
-    addArt(g) {
-        this.data.artCount++;
-        this.data.artTotalG += g;
-        this.saveData();
-    }
-
-    addSanku() {
-        this.data.sankuCount++;
-        this.saveData();
-    }
-
-    addBurst() {
-        this.data.burstCount++;
-        this.saveData();
-    }
-
-    addChallenge() {
-        this.data.challengeCount++;
-        this.saveData();
-    }
-
-    addChallengeWin() {
-        this.data.challengeWinCount++;
-        this.saveData();
-    }
+    addCredit(amount) { this.data.credit += amount; this.saveData(); }
+    addBig() { this.data.bigCount++; this.saveData(); }
+    addReg() { this.data.regCount++; this.saveData(); }
+    addEpisode() { this.data.episodeCount++; this.saveData(); }
+    addArt(g) { this.data.artCount++; this.data.artTotalG += g; this.saveData(); }
+    addSanku() { this.data.sankuCount++; this.saveData(); }
+    addBurst() { this.data.burstCount++; this.saveData(); }
+    addChallenge() { this.data.challengeCount++; this.saveData(); }
+    addChallengeWin() { this.data.challengeWinCount++; this.saveData(); }
 
     getStats() {
-        const percentage = this.data.totalBet > 0 
+        const percentage = this.data.totalBet > 0
             ? ((this.data.totalPayout / this.data.totalBet) * 100).toFixed(2)
             : '0.00';
-        
         const avgArt = this.data.artCount > 0
             ? (this.data.artTotalG / this.data.artCount).toFixed(1)
             : '0.0';
-
         return {
             totalGames: this.data.totalGames,
             totalBet: this.data.totalBet,
             totalPayout: this.data.totalPayout,
-            percentage: percentage,
+            percentage,
             credit: this.data.credit,
             bigCount: this.data.bigCount,
             regCount: this.data.regCount,
             episodeCount: this.data.episodeCount,
             artCount: this.data.artCount,
             artTotalG: this.data.artTotalG,
-            avgArt: avgArt,
+            avgArt,
             sankuCount: this.data.sankuCount,
             burstCount: this.data.burstCount,
             setting: this.data.setting,
